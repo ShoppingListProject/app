83c4e027dc8ba3e18352395f6ae5328011b6220f
this0is0kuba <jakub.rakus06@gmail.com>
2025-07-03 13:28:57 +0200

    add two main buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import logoBlack from './assets/logo-black.png';
+
 function App() {
 
+  
+
   return (
-    <>
-     <div className="text-2xl">Hello World</div>
-    </>
+    <div className="min-h-screen flex flex-col items-center flex-wrap">
+     <nav className="flex items-center flex-none">
+      <img src={logoBlack} alt="shopping list logo" className='w-50'></img>
+     </nav>
+      <main className="flex flex-col sm:flex-row grow mb-40 justify-center items-center h-full gap-20">      
+        <button className="size-50 bg-blue-800 text-white rounded hover:bg-blue-900 cursor-pointer">
+          Generate Shopping List
+        </button>
+        <button className="size-50 bg-blue-800 text-white rounded hover:bg-blue-900 cursor-pointer">
+          Add New Dish
+        </button>
+      </main>
+    </div>
   )
 }
 
