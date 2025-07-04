2f6dd7a4b3d4901c6dc52daa5c1ced2f61cc9e90
this0is0kuba <jakub.rakus06@gmail.com>
2025-07-04 18:33:08 +0200

    fix button position

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
           <div className='h-5 mt-5 mb-5 bg-blue-950 rounded-full'></div>
           <span className='text-xl'>Create your perfect shopping list by selecting dishes from the catalog or adding your own. Each dish includes its ingredients, and once selected, a categorized shopping list is generated automatically. Easily manage, save, or copy your list — shopping made simple.</span>
         </div>
-        <div className='flex flex-row w-full justify-center mt-10'>
-          <button className="font-bold shadow-lg w-50 h-30 bg-blue-300 rounded-full hover:bg-blue-400 border-3 border-blue-950 cursor-pointer">
+        <div className='mt-10 block flex justify-center items-center w-full md:block'>
+          <button className="font-bold shadow-lg w-60 h-30 bg-blue-300 rounded-lg hover:bg-blue-400 border-3 border-blue-950 cursor-pointer">
             Generate Shopping List
           </button>
         </div>
