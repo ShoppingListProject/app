5e6a186a35b084548655a6e10587d7369866d97b
this0is0kuba <jakub.rakus06@gmail.com>
2025-07-12 19:30:56 +0200

    add pagination

--- src/components/shopping-lists/ShoppingLists.tsx
+++ src/components/shopping-lists/ShoppingLists.tsx
@@ -1,4 +1,4 @@
-import { TrashIcon } from "@heroicons/react/16/solid";
+import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "@heroicons/react/16/solid";
 import { EyeIcon, MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 
 function ShoppingLists() {
@@ -7,7 +7,9 @@ function ShoppingLists() {
         <div className="bg-blue-100 w-lg md:w-2xl lg:w-4xl p-5 rounded-lg shadow-lg sm:m-5 mt-10">
           <h1 className="text-center text-3xl"> List Manager </h1>
           <div className="flex flex-col justify-center mt-5 gap-5">
+
             <div className="border-b-4 rounded"></div>
+
             <div className="shadow-lg rounded-lg grow border">
               <div>
                 <label className="flex items-center gap-2 p-2">
@@ -18,6 +20,7 @@ function ShoppingLists() {
                 </label>
               </div>
             </div>
+            
               <table>
                   <thead>
                     <tr className="bg-blue-200 border-b-2">
@@ -28,15 +31,17 @@ function ShoppingLists() {
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="p-2 text-center">Weekend Shopping</td>
+                      <td className="p-2 text-center">
+                        <button className="cursor-pointer p-2" >Weekend Shopping</button>
+                      </td>
                       <td className="p-2 text-center">2023-10-01</td>
                       <td className="p-2 text-center align-middle">
                         <div className="flex flex-col sm:flex-row items-center justify-center gap-2">
-                          <button className="bg-blue-300 p-1 rounded hover:bg-blue-400 block">
+                          <button className="bg-blue-300 p-1 rounded hover:bg-blue-400 block cursor-pointer">
                             <span className="hidden sm:inline">Show</span>
                             <EyeIcon className="h-5 w-5 sm:hidden" />
                           </button>
-                          <button className="bg-red-300 p-1 rounded hover:bg-red-400 block">
+                          <button className="bg-red-300 p-1 rounded hover:bg-red-400 block cursor-pointer">
                             <span className="hidden sm:inline" >Delete</span>
                             <TrashIcon className="h-5 w-5 sm:hidden" />
                           </button>
@@ -46,6 +51,17 @@ function ShoppingLists() {
                     <tr className="border-b-2" ></tr>
                   </tbody>
                 </table>
+
+                <div className="flex justify-center mt-5 gap-2 items-center">
+                  <button className="bg-blue-300 p-2 rounded hover:bg-blue-400 h-10 w-10 cursor-pointer">
+                    <ArrowLeftIcon/>
+                  </button>
+                  <span>1/20</span>
+                  <button className="bg-blue-300 p-2 rounded hover:bg-blue-400 h-10 w-10 cursor-pointer">
+                    <ArrowRightIcon/>
+                  </button>
+                </div>
+
           </div>
         </div>
     </div>
