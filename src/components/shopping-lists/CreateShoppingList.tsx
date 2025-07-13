8f3fd2d2e47b4e7ebe5cc8398f1ed3469c60c4db
this0is0kuba <jakub.rakus06@gmail.com>
2025-07-13 12:20:12 +0200

    add create-list page

--- src/components/shopping-lists/CreateShoppingList.tsx
+++ src/components/shopping-lists/CreateShoppingList.tsx
@@ -1,9 +1,14 @@
 import PageContent from "../shared/PageContent";
+import Pagination from "../shared/table/Pagination";
+import SearchInput from "../shared/table/SearchInput";
+import CreationTable from "./create-shopping-list/CreationTable";
 
 function CreateShoppingList() {
   return (
     <PageContent title="Create Shopping List">
-      <div></div>
+      <SearchInput placeholder="Spaghetti"/>
+      <CreationTable/>
+      <Pagination />
     </PageContent>
   )
 }
---
+++ src/components/shopping-lists/create-shopping-list/CreationTable.tsx
@@ -0,0 +1,35 @@
+import {MinusIcon, PlusIcon } from "@heroicons/react/16/solid";
+
+function CreationTable() {
+  return (
+    <table>
+      <thead>
+          <tr className="bg-blue-200 border-b-2">
+          <th className="p-2 w-2/3">Recipe Name</th>
+          <th className="p-2 w-1/3">Amount</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="p-2 text-center">
+              <button className="cursor-pointer p-2" >Spaghetti</button>
+          </td>
+          <td className="p-2 text-center align-middle">
+            <div className="flex justify-center items-center gap-2">
+              <button className="bg-blue-300 p-1 rounded hover:bg-blue-400 block cursor-pointer">
+                  <PlusIcon className="h-5 w-5" />
+              </button>
+              <span>0</span>
+              <button className="bg-red-300 p-1 rounded hover:bg-red-400 block cursor-pointer">
+                  <MinusIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </td>
+        </tr>
+        <tr className="border-b-2" ></tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default CreationTable;
